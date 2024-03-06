cff433b62e0eeb2182eace2214765e125cfde108
Ron Yaacov <ronyaacov19@gamil.com>
2024-03-06 12:01:30 +0200

    git ignore abd encoder decoder

--- src/main/java/bgu/spl/net/impl/tftp/TftpEncoderDecoder.java
+++ src/main/java/bgu/spl/net/impl/tftp/TftpEncoderDecoder.java
@@ -3,19 +3,37 @@ package bgu.spl.net.impl.tftp;
 import bgu.spl.net.api.MessageEncoderDecoder;
 
 public class TftpEncoderDecoder implements MessageEncoderDecoder<byte[]> {
-    //TODO: Implement here the TFTP encoder and decoder
-
+    private byte[] bytes = new byte[1 << 10];
+    private int len = 0;
 
     @Override
     public byte[] decodeNextByte(byte nextByte) {
-        // TODO: implement this
-
+        if(nextByte == 0) {
+            return popString();
+        }
+        pushByte(nextByte);
         return null;
     }
 
     @Override
     public byte[] encode(byte[] message) {
-        //TODO: implement this
-        return null;
+        byte[] result = new byte[message.length + 1];
+        System.arraycopy(message, 0, result, 0, message.length);
+        result[message.length] = 0;
+        return result;
+
+    }
+
+    private void pushByte(byte nextByte) {
+        if (len >= bytes.length) {
+            bytes = java.util.Arrays.copyOf(bytes, len * 2);
+        }
+        bytes[len++] = nextByte;
+    }
+
+    private byte[] popString() {
+        byte[] result = java.util.Arrays.copyOf(bytes, len);
+        len = 0;
+        return result;
     }
 }
